c6bac887a1d8341494f9b9b9b2096764af8138d0
bitmagier <bitmagier@mailbox.org>
2023-04-30 21:00:31 +0200

    towards game keras model

--- python/keras_model/create_game_keras_model_1.py
+++ python/keras_model/create_game_keras_model_1.py
@@ -8,11 +8,15 @@ from keras import layers, models
 class GameModel1(models.Sequential):
     def __init__(self, *args, **kwargs):
         super(GameModel1, self).__init__(*args, **kwargs)
+        # input: batches of normalized 2D Game screenshots (`tf.random.normal`)
+        # effective input_shape: (batch-size, x, y, color-channels)
+
         # TODO dimensions
         # filters: integer, the dimensionality of the output space (i.e. the number of output filters in the convolution).
         #          initial filters in a network is responsible for detecting edges and blobs; 16 or 32 is recommended
         # kernel_size
-        # input_shape: (x, y, color-channels)
+        #
+        self.train_batch_size = 100
         self.add(layers.Conv2D(16, (5, 5), activation='relu', input_shape=(600, 800, 3)))
         self.add(layers.MaxPooling2D((3, 3)))
         self.add(layers.Conv2D(32, (5, 5), activation='relu'))
@@ -36,15 +40,19 @@ class GameModel1(models.Sequential):
         self.summary()
 
     @tf.function
-    def call(self, inputs):
+    def x_call(self, inputs):
         output = self(inputs)
         return output
 
     @tf.function
-    def train(self, train_data):
+    def x_training(self, train_data):
         loss = self.train_step(train_data)['loss']
         return loss
 
+
+# (1)[https://www.tensorflow.org/guide/keras/sequential_model]
+# (2)[https://keras.io/guides/sequential_model/#:~:text=A%20Sequential%20model%20is%20appropriate,tensor%20and%20one%20output%20tensor.&text=A%20Sequential%20model%20is%20not,multiple%20inputs%20or%20multiple%20outputs]
+# (3)[https://pyimagesearch.com/2019/10/28/3-ways-to-create-a-keras-model-with-tensorflow-2-0-sequential-functional-and-model-subclassing/]
 
 model = GameModel1()
 
@@ -56,20 +64,23 @@ model.compile(optimizer='adam',
 
 # Get concrete function for the call and training method
 # TODO add batch size as first parameter for `shape` (see conv2d.py)
-pred_output = model.call.get_concrete_function(tf.TensorSpec(shape=[600, 800, 3],
-                                                             dtype=tf.float32,
-                                                             name='inputs'))
+pred_output = model.x_call.get_concrete_function(tf.TensorSpec(shape=[None, 600, 800, 3],
+                                                               dtype=tf.float32,
+                                                               name='inputs'))
 
-train_output = model.training.get_concrete_function((tf.TensorSpec(shape=[600, 800, 3],
-                                                                   dtype=tf.float32, name="training_input"),
-                                                     tf.TensorSpec(shape=[1],
-                                                                   dtype=tf.float32,
-                                                                   name="training_target")))
+# train_output = model.x_training.get_concrete_function((tf.TensorSpec(shape=[model.train_batch_size, 600, 800, 3],
+#                                                                      dtype=tf.float32, name="training_input"),
+#                                                        tf.TensorSpec(shape=[1],
+#                                                                      dtype=tf.float32,
+#                                                                      name="training_target")))
 
 # Save the model
+# model.save('game_keras_model_1',
+#            save_format='tf',
+#            signatures={'train': train_output, 'pred': pred_output})
 model.save('game_keras_model_1',
            save_format='tf',
-           signatures={'train': train_output, 'pred': pred_output})
+           signatures={'pred': pred_output})
 
 # one way to get output names via saved_model_cli:
 # saved_model_cli show --dir /path/to/saved-model/ --all
@@ -86,3 +97,6 @@ model.save('game_keras_model_1',
 #           against the old model, but train the new one
 #   - replace old model by the new model
 #   - return stats (loss, etc)
+
+
+# TODO were are the training parameter come from in the convolutional step?
