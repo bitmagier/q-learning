ee18d239825da1999acb0c60fe78e3bf7a32037d
bitmagier <bitmagier@mailbox.org>
2023-05-20 08:46:51 +0200

    .

---
+++ src/ai/learn_the_game.rs
@@ -0,0 +1,6 @@
+
+type Result<T> = std::result::Result<T, dyn std::error::Error>;
+
+fn learn_until_fit() -> Result<()> {
+    todo!()
+}
--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,2 +1,2 @@
-pub mod interface;
+pub mod learn_the_game;
 mod q_learning_model_1;
