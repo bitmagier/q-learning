5eb98803416f066255bb0ffd0c78c9c8ac48a95d
bitmagier <bitmagier@mailbox.org>
2023-06-06 23:11:22 +0200

    tensorflow keras model prototype attached

--- src/ai/realtime_ai_player.rs
+++ src/ai/realtime_ai_player.rs
@@ -1,10 +1,11 @@
 #![allow(unused)]
 
+use futures::channel::mpsc::Sender;
 use image::{ImageBuffer, Luma, Pixel};
 use lazy_static::lazy_static;
 use tensorflow::Tensor;
-use crate::ai::model::q_learning_tf_model1::{QLearningTfModel1, WORLD_STATE_NUM_FRAMES};
 
+use crate::ai::model::q_learning_tf_model1::{QLearningTfModel1, WORLD_STATE_NUM_FRAMES};
 use crate::app::{FRAME_SIZE_X, FRAME_SIZE_Y};
 use crate::breakout::mechanics::{GameInput, PanelControl};
 
@@ -17,35 +18,16 @@ lazy_static!(
 );
 
 
-// #[derive(Clone)]
-// pub struct GrayFrame([u8; (FRAME_SIZE_X * FRAME_SIZE_Y) as usize]);
-//
-// impl GrayFrame {
-//     pub fn from_rgb(frame: [u8; (FRAME_SIZE_X * FRAME_SIZE_Y * 3) as usize]) -> Self {
-//         let mut r = [0_u8; (FRAME_SIZE_X * FRAME_SIZE_Y) as usize];
-//         for (i, pixel) in frame.chunks(3).enumerate() {
-//             let gray_value = ((pixel[0] as usize + pixel[1] as usize + pixel[2] as usize) / 3) as u8;
-//             r[i] = gray_value;
-//         }
-//         GrayFrame(r)
-//     }
-// }
-//
-// impl Default for GrayFrame {
-//     fn default() -> Self {
-//         Self([0; (FRAME_SIZE_X * FRAME_SIZE_Y) as usize])
-//     }
-// }
-
 type GrayFrame = ImageBuffer<Luma<u8>, Vec<u8>>;
 
 #[allow(unused_must_use)]
-struct GrayFrameRingBuffer {
+struct FrameRingBuffer {
     /// four most recent frames
     buffer: [GrayFrame; WORLD_STATE_NUM_FRAMES as usize],
     next_slot: usize,
 }
-impl GrayFrameRingBuffer {
+
+impl FrameRingBuffer {
     pub fn add(&mut self, element: GrayFrame) {
         self.buffer[self.next_slot] = element;
         self.next_slot = match self.next_slot + 1 {
@@ -74,7 +56,7 @@ impl GrayFrameRingBuffer {
 
     fn get(&self, steps_into_history: usize) -> &GrayFrame {
         assert!(steps_into_history < 4, "available steps into history: 0..3");
-        let slot =  match self.next_slot as isize - 1 - steps_into_history as isize {
+        let slot = match self.next_slot as isize - 1 - steps_into_history as isize {
             slot @ 0.. => slot as usize,
             slot @ _ => (slot + 4) as usize
         };
@@ -82,7 +64,7 @@ impl GrayFrameRingBuffer {
     }
 }
 
-impl Default for GrayFrameRingBuffer {
+impl Default for FrameRingBuffer {
     fn default() -> Self {
         Self {
             buffer: [GrayFrame::default(), GrayFrame::default(), GrayFrame::default(), GrayFrame::default()],
@@ -92,38 +74,40 @@ impl Default for GrayFrameRingBuffer {
 }
 
 pub struct RealtimeAiPlayer {
-    frames: GrayFrameRingBuffer,
-    model: QLearningTfModel1
+    frames: FrameRingBuffer,
+    model: QLearningTfModel1,
+    action_sender: Sender<GameInput>,
 }
 
 impl RealtimeAiPlayer {
-    pub fn new() -> Self {
+    pub fn new(action_sender: Sender<GameInput>) -> Self {
         Self {
-            frames: GrayFrameRingBuffer::default(),
-            model: QLearningTfModel1::init()
+            frames: FrameRingBuffer::default(),
+            model: QLearningTfModel1::init(),
+            action_sender,
         }
     }
 
-    pub async fn watch_next_frame(&mut self, frame: ImageBuffer<Luma<u8>, Vec<u8>>) -> GameInput {
+    pub fn watch_next_frame(&mut self, frame: ImageBuffer<Luma<u8>, Vec<u8>>) {
         self.frames.add(frame);
-        // TODO run AI model `predict()`
+        // TODO run model predict function if no such one is already running
         todo!()
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::ai::realtime_ai_player::RealtimeAiPlayer;
+    use crate::ai::realtime_ai_player::FrameRingBuffer;
 
     #[test]
     fn test_tensor_linear_storage_assumptions() {
-        let t = RealtimeAiPlayer::new();
-        assert_eq!(t.frames.as_tensor().get_index(&[0, 0, 0]), 0);
-        assert_eq!(t.frames.as_tensor().get_index(&[0, 0, 1]), 1);
-        assert_eq!(t.frames.as_tensor().get_index(&[0, 0, 2]), 2);
-        assert_eq!(t.frames.as_tensor().get_index(&[0, 1, 0]), 4);
-        assert_eq!(t.frames.as_tensor().get_index(&[0, 1, 1]), 5);
-        assert_eq!(t.frames.as_tensor().get_index(&[1, 0, 0]), 800 * 4);
-        assert_eq!(t.frames.as_tensor().get_index(&[1, 1, 0]), 800 * 4 + 4);
+        let b = FrameRingBuffer::default();
+        assert_eq!(b.as_tensor().get_index(&[0, 0, 0]), 0);
+        assert_eq!(b.as_tensor().get_index(&[0, 0, 1]), 1);
+        assert_eq!(b.as_tensor().get_index(&[0, 0, 2]), 2);
+        assert_eq!(b.as_tensor().get_index(&[0, 1, 0]), 4);
+        assert_eq!(b.as_tensor().get_index(&[0, 1, 1]), 5);
+        assert_eq!(b.as_tensor().get_index(&[1, 0, 0]), 800 * 4);
+        assert_eq!(b.as_tensor().get_index(&[1, 1, 0]), 800 * 4 + 4);
     }
 }
--- src/game_ai_adapter.rs
+++ src/game_ai_adapter.rs
@@ -1,39 +1,37 @@
+use futures::channel::mpsc;
+use futures::channel::mpsc::Receiver;
 use image::{ImageBuffer, imageops, Rgb};
 
 use crate::ai::realtime_ai_player::RealtimeAiPlayer;
 use crate::app::ExternalGameController;
-use crate::breakout::mechanics::GameInput;
+use crate::breakout::mechanics::{GameInput, PanelControl};
 
-use threadpool::ThreadPool;
-
-// TODO asynchronously run AI model (playing or learning mode)
+// TODO asynchronously run AI model (in learning mode / or in playing mode with a pre-trained model)
 pub struct GameAiAdapter {
     player: RealtimeAiPlayer,
-    next_action: Option<GameInput>
+    action_receiver: Receiver<GameInput>,
 }
+
 impl GameAiAdapter {
     pub fn new() -> Self {
-        todo!()
+        let (action_sender, action_receiver) = mpsc::channel(1);
+        Self {
+            player: RealtimeAiPlayer::new(action_sender),
+            action_receiver,
+        }
     }
 }
+
 impl ExternalGameController for GameAiAdapter {
     fn show_frame(&mut self, frame: ImageBuffer<Rgb<u8>, Vec<u8>>) {
-
-
-        let game_input = self.player.watch_next_frame(
-            imageops::grayscale(&frame)
-        );
-
-        // TODO run tensorflow model and fill `next_action`
-        todo!()
+        self.player.watch_next_frame(imageops::grayscale(&frame))
     }
 
     fn read_input(&mut self) -> GameInput {
-        if let Some(action) = self.next_action {
-            self.next_action = None;
-            action
-        } else {
-            GameInput::none()
+        match self.action_receiver.try_next() {
+            Ok(Some(action)) => action,
+            Ok(None) => GameInput { control: PanelControl::None, exit: true }, // AI player thread stopped unexpectedly
+            Err(_) => GameInput::none()
         }
     }
 }
