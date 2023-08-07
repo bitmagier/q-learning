51b144c93b3f80dc0f4a1db3ee827e61116786e7
bitmagier <bitmagier@mailbox.org>
2023-08-07 15:35:28 +0200

    model traits cleanup

--- src/q-learning/src/ql/ml_model/tensorflow_python/mod.rs
+++ src/q-learning/src/ql/ml_model/tensorflow_python/mod.rs
@@ -1,2 +1,5 @@
+//! Q-Learning implemented in Rust and driven by Rust code - utilizing a SavedModel defined via Python + Keras  
+//! Depends on python code in folder `q-learning/python_model`
+
 pub mod model_function;
 pub mod q_learning_model;
